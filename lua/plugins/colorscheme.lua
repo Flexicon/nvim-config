a99522b0f62ec38950f436547b3d95053b824167
Mike Repec <mikerepec@gmail.com>
2025-09-20 21:10:37 +0200

    fix: remove workarounds and version pins for mason and catppuccin

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,7 @@
 -- Configure LazyVim colorscheme
 return {
   -- add colorscheme plugin
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    -- Pinned to a specific commit to avoid breaking changes around the `palettes.get` function.
-    -- Can unpin once this commit is included in a release:
-    -- https://github.com/catppuccin/nvim/commit/30fa4d122d9b22ad8b2e0ab1b533c8c26c4dde86
-    version = "v1.11.0",
-  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- Configure LazyVim to use said colorscheme
   {
