93dc44474b680be879cc6c2ed7d55edf18018987
Mike Repeć <mikerepec@pm.me>
2026-05-01 11:49:16 +0200

    feat(conform): configure biome formatter with fallback to prettier

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,9 +1,19 @@
+local frontend_formatters = { "biome", "prettier", stop_after_first = true }
+
 return {
   -- Configure conform.nvim to use ruff for Python formatting
   {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        javascript = frontend_formatters,
+        javascriptreact = frontend_formatters,
+        typescript = frontend_formatters,
+        typescriptreact = frontend_formatters,
+        css = frontend_formatters,
+        html = frontend_formatters,
+        json = frontend_formatters,
+        yaml = frontend_formatters,
         python = { "ruff_format" },
       },
     },
